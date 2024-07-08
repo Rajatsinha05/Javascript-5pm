700c64a7bb04dec04bdf20bd75d5f5ff42163b6b
Rajat sinha <rajatsinha5467@gmail.com>
2024-07-08 18:06:21 +0530

    crud

--- json-server/api/product.api.js
+++ json-server/api/product.api.js
@@ -5,12 +5,30 @@ export const createProduct = async (product) => {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(product)
     })
-    let res=await req.json()
+    let res = await req.json();
+}
+
+export const getProducts = async () => {
+    let req = await fetch('http://localhost:3000/products');
+    let res = await req.json()
+    return res
 }
 
 
-export const getProducts = async ()=>{
-    let req = await fetch('http://localhost:3000/products')
-    let res = await req.json()
-    console.log(res);
+
+export const DeleteProduct = async (id) => {
+
+    let req = await fetch(`http://localhost:3000/products/${id}`, {
+        method: 'DELETE'
+    })
+}
+
+
+
+export const updateProduct = async (id, product) => {
+    let req = await fetch(`http://localhost:3000/products/${id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(product)
+    })
 }
